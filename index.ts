d1d87d252504fda961ceb73821c9ba66f787a016
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-02-18 17:45:04 +0100

    Adding SQL not finished yet

--- index.ts
+++ index.ts
@@ -1,142 +1,35 @@
 import express from 'express'
 import cors from 'cors'
+import Database from 'better-sqlite3'
 
+const db = new Database('./quotes_data.db', {
+    verbose: console.log
+})
 const app = express()
 app.use(cors())
 app.use(express.json())
 const PORT = 3001
 
-type Quote = {
-    id: number
-    content: string
-    authorId: number
-}
+const getAllAuthours = db.prepare(`SELECT * FROM authors;`);
+const getAllQuotesId = db.prepare(`SELECT _id FROM quotes;`);
+const getAllQuotesAndAuthors = db.prepare(`
+SELECT quotes._id,
+    quotes.content,
+    authors.firstName,
+    authors.lastName,
+    authors.age,
+    authors.image,
+    authors.dead FROM quotes INNER JOIN authors ON authorId = authors._id ;
+`);
 
-type Author = {
-    id: number,
-    firstName: string
-    lastName: string
-    age: number
-    image: string
-    dead: boolean
-}
-
-type NewQuote = {
-    content: string
-    authorId: number
-}
-
-let authors: Author[] = [
-    {
-        'id': 1,
-        'firstName': 'Mark',
-        'lastName': 'Twain',
-        'age': 74,
-        'image': "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSbWpXNvj-m3_xaQm4m9ALrf3c1D-KscNie2FOxi42FjzMZ5gxf",
-        'dead': true
-    },
-    {
-        'id': 2,
-        'firstName': 'Albert',
-        'lastName': 'Einstein',
-        'age': 76,
-        'image': "https://parade.com/wp-content/uploads/2021/08/albert-einstein-quotes.jpg",
-        'dead': true
-    },
-    {
-        'id': 3,
-        'firstName': 'Albert',
-        'lastName': 'Camus',
-        'age': 46,
-        'image': "https://media.newyorker.com/photos/5909675d019dfc3494ea0dd0/16:9/w_1280,c_limit/120409_r22060_g2048.jpg",
-        'dead': true
-    },
-    {
-        'id': 4,
-        'firstName': 'Thomas',
-        'lastName': 'Stearns Eliot',
-        'age': 76,
-        'image': "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcT7s0yhE2NW95VH829FAn236XjX-cg9fAO8LJxG7mzc-aAkDa4DvgP5Q7H5lhSl",
-        'dead': true
-    },
-    {
-        'id': 5,
-        'firstName': 'Margaret',
-        'lastName': 'Atwood',
-        'age': 82,
-        'image': "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRZWkYKxRmvp29kWgKsvBWSBDrxWK_OpPEFs9yMP7doszUewZvH",
-        'dead': false
-    },
-    {
-        'id': 6,
-        'firstName': "Ryan",
-        'lastName': 'Dahl (Creator of Node JS)',
-        'age': 41,
-        'image': "https://www.mappingthejourney.com/wp-content/uploads/2017/08/image.jpg",
-        'dead': false
-    },
-    {
-        'id': 7,
-        'firstName': 'Henry',
-        'lastName': 'David Thoreau',
-        'age': 44,
-        'image': "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSWrCRzadHKIofduRnG4lVp5WVnMNjZuHf654yg15SQ8VB8VAQoL8uEm0CQ34ir",
-        'dead': true
-    },
-    {
-        'id': 8,
-        'firstName': 'Stephen',
-        'lastName': 'King',
-        'age': 74,
-        'image': "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFNn6FcqJgnTCBDjX9vK_qRnle-dHO1Jp7uEEqISfNOm8bbjoW",
-        'dead': false
-    }
-]
-
-let quotes: Quote[] = [
-    {
-        'id': 1,
-        'content': 'Make memes, not war!',
-        'authorId': 1
-    },
-    {
-        'id': 2,
-        'content': 'Stop making quotes I never said!',
-        'authorId': 2
-    },
-    {
-        'id': 3,
-        'content': "The struggle itself towards the heights it's enough to fill a man's heart.",
-        'authorId': 3
-    },
-    {
-        'id': 4,
-        'content': "We must never cease from exploring. And the end of our exploring will be to arrive where we began and know the place for the first time.",
-        'authorId': 4
-    },
-    {
-        'id': 5,
-        'content': "Ignoring isn't the same as ignorance, you have to work at it.",
-        'authorId': 5
-    },
-    {
-        'id': 6,
-        'content': "You can never understand everything. But, you should push yourself to understand the system.",
-        'authorId': 6
-    },
-    {
-        'id': 7,
-        'content': 'Success usually comes to those who are too busy to be looking for it.',
-        'authorId': 7
-    },
-    {
-        'id': 8,
-        'content': 'Get busy living or get busy dying.',
-        'authorId': 8
-    }
-]
-
-
+const getQuoteAndAuthorById = db.prepare(`
+SELECT quotes._id,
+    quotes.content,
+    authors.firstName,
+    authors.lastName,
+    authors.age,
+    authors.image,
+    authors.dead FROM quotes INNER JOIN authors ON authorId = authors._id WHERE quotes._id=?;`);
 
 app.get('/', function (req, res) {
     res.send(`
@@ -144,12 +37,14 @@ app.get('/', function (req, res) {
     <p>Here are some endpoints you can use:</p>
     <ul>
       <li><a href="/quotes">/quotes</a></li>
+      <li><p> You can use (/quotes?serch=string) for searching inside quote content. </p></li>
       <li><a href="/randomQuote">/randomQuote</a></li>
     </ul>
    `)
 })
 
 app.get('/quotes', function (req, res) {
+    const quotes = getAllQuotesAndAuthors.all();
     const search = req.query.search
     let quotesToSend = quotes
 
@@ -158,225 +53,220 @@ app.get('/quotes', function (req, res) {
             quote.content.toLowerCase().includes(search.toLowerCase())
         )
     }
-
-    let quotesToSendCopy = JSON.parse(JSON.stringify(quotesToSend))
-
-    for (const quote of quotesToSendCopy) {
-        const author = authors.find(author => author.id === quote.authorId)
-        quote.author = author
-    }
-
-    res.send(quotesToSendCopy)
-})
-
-app.get('/randomQuote', function (req, res) {
-    const randomIndex = Math.floor(Math.random() * quotes.length)
-    res.send(quotes[randomIndex])
+    res.send(quotesToSend)
 })
 
 app.get('/quotes/:id', function (req, res) {
     const id = Number(req.params.id)
+    const result = getQuoteAndAuthorById.get(id)
 
-    const match = quotes.find(person => person.id === id)
-    if (match) {
-        res.send(match)
+    if (result) {
+        res.send(result)
     } else res.status(404).send({ error: 'Quote not found' })
 })
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-
-    const { content, authorId } = req.body
-
-    const errors = []
-
-    if (typeof content !== 'string') {
-        errors.push(`Content missing or not a string`)
-    }
-    if (typeof authorId !== 'number') {
-        errors.push(`authorId missing or not a number`)
-    }
-
-
-    if (errors.length === 0) {
-        const newQuote = {
-            id: Math.random(),
-            content,
-            authorId
-        }
-        quotes.push(newQuote)
-        res.status(201).send(newQuote)
-    } else {
-        res.status(400).send({ errors: errors })
-    }
+app.get('/randomQuote', function (req, res) {
+    const quotes = getAllQuotesId.all();
+    const randomIndex = Math.floor(Math.random() * (quotes.length - 1) + 1)
+    // console.log(randomIndex) 
+    const result = getQuoteAndAuthorById.get(randomIndex)
+    res.send(result)
 })
 
-app.patch('/quotes/:id', (req, res) => {
-    const id = Number(req.params.id)
+// app.post('/quotes', (req, res) => {
+//     console.log(req.body)
 
-    const { content, authorId }: NewQuote = req.body
+//     const { content, authorId } = req.body
 
-    const quoteToChange = quotes.find(quote => quote.id === id)
+//     const errors = []
 
-    const errors = []
+//     if (typeof content !== 'string') {
+//         errors.push(`Content missing or not a string`)
+//     }
+//     if (typeof authorId !== 'number') {
+//         errors.push(`authorId missing or not a number`)
+//     }
 
-    if (quoteToChange) {
-        // if (typeof firstName === 'string') {
-        //     quoteToChange.firstName = firstName
-        // } else errors.push(`First name not a string`)
-        // if (typeof lastName === 'string') {
-        //     quoteToChange.lastName = lastName
-        // } else errors.push(`Last name not a string`)
-        if (typeof content === 'string') {
-            quoteToChange.content = content
-        } else errors.push(`Content not a string`)
-        if (typeof authorId === 'number') {
-            quoteToChange.authorId = authorId
-        } else errors.push(`authorId not a number`)
-        // if (typeof image === 'string') {
-        //     quoteToChange.image = image
-        // } else (`Imagee not a string`)
-        // if (typeof age === 'number') {
-        //     quoteToChange.age = age
-        // } else errors.push(`Age not a number`)
-        // if (typeof dead === 'boolean') {
-        //     quoteToChange.dead = dead
-        // } else errors.push(`Dead not a boolean`)
-        res.send({ data: quoteToChange, errors: errors })
-    } else {
-        res.status(404).send({ error: 'Quote not found.' })
-    }
-})
 
-app.delete('/quotes/:id', (req, res) => {
-    const id = Number(req.params.id)
+//     if (errors.length === 0) {
+//         const newQuote = {
+//             id: Math.random(),
+//             content,
+//             authorId
+//         }
+//         quotes.push(newQuote)
+//         res.status(201).send(newQuote)
+//     } else {
+//         res.status(400).send({ errors: errors })
+//     }
+// })
 
-    const match = quotes.find(quote => quote.id === id)
+// app.patch('/quotes/:id', (req, res) => {
+//     const id = Number(req.params.id)
 
-    if (match) {
-        quotes = quotes.filter(quote => quote.id !== id)
-        res.send({ messsage: "Quote deleted sucessfully" })
-    } else {
-        res.status(404).send({ error: 'Qoute not found' })
-    }
-})
+//     const { content, authorId }: NewQuote = req.body
 
-app.get('/authors', (req, res) => {
-    const search = req.query.search
-    let authorsToSend = authors
+//     const quoteToChange = quotes.find(quote => quote.id === id)
 
-    if (typeof search === 'string') {
-        authorsToSend = authorsToSend.filter(author =>
-            author.firstName.toLowerCase().includes(search.toLowerCase()) ||
-            author.lastName.toLowerCase().includes(search.toLowerCase())
-        )
-    }
-    res.send(authorsToSend)
-})
+//     const errors = []
 
-app.get('/authors/:id', function (req, res) {
-    const id = Number(req.params.id)
+//     if (quoteToChange) {
+//         // if (typeof firstName === 'string') {
+//         //     quoteToChange.firstName = firstName
+//         // } else errors.push(`First name not a string`)
+//         // if (typeof lastName === 'string') {
+//         //     quoteToChange.lastName = lastName
+//         // } else errors.push(`Last name not a string`)
+//         if (typeof content === 'string') {
+//             quoteToChange.content = content
+//         } else errors.push(`Content not a string`)
+//         if (typeof authorId === 'number') {
+//             quoteToChange.authorId = authorId
+//         } else errors.push(`authorId not a number`)
+//         // if (typeof image === 'string') {
+//         //     quoteToChange.image = image
+//         // } else (`Imagee not a string`)
+//         // if (typeof age === 'number') {
+//         //     quoteToChange.age = age
+//         // } else errors.push(`Age not a number`)
+//         // if (typeof dead === 'boolean') {
+//         //     quoteToChange.dead = dead
+//         // } else errors.push(`Dead not a boolean`)
+//         res.send({ data: quoteToChange, errors: errors })
+//     } else {
+//         res.status(404).send({ error: 'Quote not found.' })
+//     }
+// })
 
-    const match = authors.find(person => person.id === id)
-    if (match) {
-        res.send(match)
-    } else res.status(404).send({ error: 'Author not found' })
-})
+// app.delete('/quotes/:id', (req, res) => {
+//     const id = Number(req.params.id)
 
-app.post('/authors', (req, res) => {
-    console.log(req.body)
+//     const match = quotes.find(quote => quote.id === id)
 
-    const { firstName,
-        lastName,
-        image,
-        age,
-        dead } = req.body
+//     if (match) {
+//         quotes = quotes.filter(quote => quote.id !== id)
+//         res.send({ messsage: "Quote deleted sucessfully" })
+//     } else {
+//         res.status(404).send({ error: 'Qoute not found' })
+//     }
+// })
 
-    const errors = []
+// app.get('/authors', (req, res) => {
+//     const search = req.query.search
+//     let authorsToSend = authors
 
-    if (typeof firstName !== 'string') {
-        errors.push(`First name missing or not a string`)
-    }
+//     if (typeof search === 'string') {
+//         authorsToSend = authorsToSend.filter(author =>
+//             author.firstName.toLowerCase().includes(search.toLowerCase()) ||
+//             author.lastName.toLowerCase().includes(search.toLowerCase())
+//         )
+//     }
+//     res.send(authorsToSend)
+// })
 
-    if (typeof lastName !== 'string') {
-        errors.push(`Last name missing or not a string`)
-    }
+// app.get('/authors/:id', function (req, res) {
+//     const id = Number(req.params.id)
 
-    if (typeof image !== 'string') {
-        errors.push(`Imagee missing or not a string`)
-    }
+//     const match = authors.find(person => person.id === id)
+//     if (match) {
+//         res.send(match)
+//     } else res.status(404).send({ error: 'Author not found' })
+// })
 
-    if (typeof age !== 'number') {
-        errors.push(`Age missing or not a number`)
-    }
+// app.post('/authors', (req, res) => {
+//     console.log(req.body)
 
-    if (typeof dead !== 'boolean') {
-        errors.push(`Dead missing or not a boolean`)
-    }
+//     const { firstName,
+//         lastName,
+//         image,
+//         age,
+//         dead } = req.body
 
-    if (errors.length === 0) {
-        const newAuthor: Author = {
-            id: Math.random(),
-            firstName,
-            lastName,
-            image,
-            age,
-            dead
-        }
-        authors.push(newAuthor)
-        res.status(201).send(newAuthor)
-    } else {
-        res.status(400).send({ errors: errors })
-    }
-})
+//     const errors = []
 
-app.patch('/authors/:id', (req, res) => {
-    const id = Number(req.params.id)
+//     if (typeof firstName !== 'string') {
+//         errors.push(`First name missing or not a string`)
+//     }
 
-    const { firstName,
-        lastName,
-        image,
-        age,
-        dead } = req.body
+//     if (typeof lastName !== 'string') {
+//         errors.push(`Last name missing or not a string`)
+//     }
 
-    const authorToChange = authors.find(author => author.id === id)
+//     if (typeof image !== 'string') {
+//         errors.push(`Imagee missing or not a string`)
+//     }
 
-    const errors = []
+//     if (typeof age !== 'number') {
+//         errors.push(`Age missing or not a number`)
+//     }
 
-    if (authorToChange) {
-        if (typeof firstName === 'string') {
-            authorToChange.firstName = firstName
-        } else errors.push(`First name not a string`)
-        if (typeof lastName === 'string') {
-            authorToChange.lastName = lastName
-        } else errors.push(`Last name not a string`)
-        if (typeof image === 'string') {
-            authorToChange.image = image
-        } else (`Imagee not a string`)
-        if (typeof age === 'number') {
-            authorToChange.age = age
-        } else errors.push(`Age not a number`)
-        if (typeof dead === 'boolean') {
-            authorToChange.dead = dead
-        } else errors.push(`Dead not a boolean`)
-        res.send({ data: authorToChange, errors: errors })
-    } else {
-        res.status(404).send({ error: 'Author not found.' })
-    }
-})
+//     if (typeof dead !== 'boolean') {
+//         errors.push(`Dead missing or not a boolean`)
+//     }
 
-app.delete('/authors/:id', (req, res) => {
-    const id = Number(req.params.id)
+//     if (errors.length === 0) {
+//         const newAuthor: Author = {
+//             id: Math.random(),
+//             firstName,
+//             lastName,
+//             image,
+//             age,
+//             dead
+//         }
+//         authors.push(newAuthor)
+//         res.status(201).send(newAuthor)
+//     } else {
+//         res.status(400).send({ errors: errors })
+//     }
+// })
 
-    const match = authors.find(author => author.id === id)
+// app.patch('/authors/:id', (req, res) => {
+//     const id = Number(req.params.id)
 
-    if (match) {
-        authors = authors.filter(author => author.id !== id)
-        res.send({ messsage: "Author deleted sucessfully" })
-    } else {
-        res.status(404).send({ error: 'Author not found' })
-    }
-})
+//     const { firstName,
+//         lastName,
+//         image,
+//         age,
+//         dead } = req.body
+
+//     const authorToChange = authors.find(author => author.id === id)
+
+//     const errors = []
+
+//     if (authorToChange) {
+//         if (typeof firstName === 'string') {
+//             authorToChange.firstName = firstName
+//         } else errors.push(`First name not a string`)
+//         if (typeof lastName === 'string') {
+//             authorToChange.lastName = lastName
+//         } else errors.push(`Last name not a string`)
+//         if (typeof image === 'string') {
+//             authorToChange.image = image
+//         } else (`Imagee not a string`)
+//         if (typeof age === 'number') {
+//             authorToChange.age = age
+//         } else errors.push(`Age not a number`)
+//         if (typeof dead === 'boolean') {
+//             authorToChange.dead = dead
+//         } else errors.push(`Dead not a boolean`)
+//         res.send({ data: authorToChange, errors: errors })
+//     } else {
+//         res.status(404).send({ error: 'Author not found.' })
+//     }
+// })
+
+// app.delete('/authors/:id', (req, res) => {
+//     const id = Number(req.params.id)
+
+//     const match = authors.find(author => author.id === id)
+
+//     if (match) {
+//         authors = authors.filter(author => author.id !== id)
+//         res.send({ messsage: "Author deleted sucessfully" })
+//     } else {
+//         res.status(404).send({ error: 'Author not found' })
+//     }
+// })
 
 app.listen(PORT, () => {
     console.log(`Server runing on: http://localhost:${PORT}/`)
---
+++ setup.ts
@@ -0,0 +1,146 @@
+import Database from "better-sqlite3";
+
+const db = new Database('./quotes_data.db', {
+    verbose: console.log
+})
+
+let authors = [
+    {
+        'firstName': 'Mark',
+        'lastName': 'Twain',
+        'age': 74,
+        'image': "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSbWpXNvj-m3_xaQm4m9ALrf3c1D-KscNie2FOxi42FjzMZ5gxf",
+        'dead': 1
+    },
+    {
+        'firstName': 'Albert',
+        'lastName': 'Einstein',
+        'age': 76,
+        'image': "https://parade.com/wp-content/uploads/2021/08/albert-einstein-quotes.jpg",
+        'dead': 1
+    },
+    {
+        'firstName': 'Albert',
+        'lastName': 'Camus',
+        'age': 46,
+        'image': "https://media.newyorker.com/photos/5909675d019dfc3494ea0dd0/16:9/w_1280,c_limit/120409_r22060_g2048.jpg",
+        'dead': 1
+    },
+    {
+        'firstName': 'Thomas',
+        'lastName': 'Stearns Eliot',
+        'age': 76,
+        'image': "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcT7s0yhE2NW95VH829FAn236XjX-cg9fAO8LJxG7mzc-aAkDa4DvgP5Q7H5lhSl",
+        'dead': 1
+    },
+    {
+        'firstName': 'Margaret',
+        'lastName': 'Atwood',
+        'age': 82,
+        'image': "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRZWkYKxRmvp29kWgKsvBWSBDrxWK_OpPEFs9yMP7doszUewZvH",
+        'dead': 0
+    },
+    {
+        'firstName': "Ryan",
+        'lastName': 'Dahl (Creator of Node JS)',
+        'age': 41,
+        'image': "https://www.mappingthejourney.com/wp-content/uploads/2017/08/image.jpg",
+        'dead': 0
+    },
+    {
+        'firstName': 'Henry',
+        'lastName': 'David Thoreau',
+        'age': 44,
+        'image': "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSWrCRzadHKIofduRnG4lVp5WVnMNjZuHf654yg15SQ8VB8VAQoL8uEm0CQ34ir",
+        'dead': 1
+    },
+    {
+        'firstName': 'Stephen',
+        'lastName': 'King',
+        'age': 74,
+        'image': "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFNn6FcqJgnTCBDjX9vK_qRnle-dHO1Jp7uEEqISfNOm8bbjoW",
+        'dead': 0
+    }
+]
+
+let quotes = [
+    {
+        'content': 'Make memes, not war!',
+        'authorId': 1
+    },
+    {
+        'content': 'Stop making quotes I never said!',
+        'authorId': 2
+    },
+    {
+        'content': "The struggle itself towards the heights it's enough to fill a man's heart.",
+        'authorId': 3
+    },
+    {
+        'content': "We must never cease from exploring. And the end of our exploring will be to arrive where we began and know the place for the first time.",
+        'authorId': 4
+    },
+    {
+        'content': "Ignoring isn't the same as ignorance, you have to work at it.",
+        'authorId': 5
+    },
+    {
+        'content': "You can never understand everything. But, you should push yourself to understand the system.",
+        'authorId': 6
+    },
+    {
+        'content': 'Success usually comes to those who are too busy to be looking for it.',
+        'authorId': 7
+    },
+    {
+        'content': 'Get busy living or get busy dying.',
+        'authorId': 8
+    }
+]
+
+const dropAuthorsTable = db.prepare("DROP TABLE authors;")
+dropAuthorsTable.run()
+const dropquotesTable = db.prepare("DROP TABLE quotes;")
+dropquotesTable.run()
+
+const createAuthorsTable = db.prepare(`
+CREATE TABLE authors (
+    _id INTEGER,
+    firstName TEXT,
+    lastName TEXT,
+    age INTEGER,
+    image TEXT,
+    dead INTEGER,
+    PRIMARY KEY (_id)
+  );
+`)
+
+const createQuotesTable = db.prepare(`
+CREATE TABLE quotes (
+    _id INTEGER,
+    content TEXT NOT NULL,
+    authorId INTEGER,
+    PRIMARY KEY(_id)
+    );
+`)
+
+createAuthorsTable.run()
+createQuotesTable.run()
+
+const deleteAuthors = db.prepare(`DELETE FROM authors;`)
+deleteAuthors.run()
+const deleteQuotes = db.prepare(`DELETE FROM quotes;`)
+deleteQuotes.run()
+
+const createAuthor = db.prepare(`INSERT INTO authors (firstName, lastName, age, image, dead) VALUES (?,?,?,?,?);`)
+const createQuote = db.prepare(`INSERT INTO quotes (content, authorId) VALUES (?,?);`)
+
+for (const author of authors) {
+    const {firstName, lastName, age, image, dead} = author
+    createAuthor.run(firstName, lastName, age, image, dead)
+}
+
+for (const quote of quotes) {
+    const {content, authorId} = quote
+    createQuote.run(content, authorId)
+}
